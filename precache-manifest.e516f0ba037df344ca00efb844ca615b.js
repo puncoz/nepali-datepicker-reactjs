f452a77d567a2c65a45a1ade1f6e40a1c5ad452e
Puncoz Nepal <puncoz138@gmail.com>
2020-07-07 01:44:30 +0545

    Updates

---
+++ precache-manifest.e516f0ba037df344ca00efb844ca615b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab33da9f8e71e46ec78bc423c3aa316d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34bae6e3b8b2c59d8be1",
+    "url": "./static/css/main.2f3d5cf9.chunk.css"
+  },
+  {
+    "revision": "4a09acb1ad6df51e8783",
+    "url": "./static/js/2.0a1164a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0a1164a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34bae6e3b8b2c59d8be1",
+    "url": "./static/js/main.82db7b80.chunk.js"
+  },
+  {
+    "revision": "b213050908112a8cdfd0",
+    "url": "./static/js/runtime-main.e5edc7d0.js"
+  },
+  {
+    "revision": "d97d73084b4ccb15bcd18c9fdc7ce11a",
+    "url": "./static/media/logo.d97d7308.svg"
+  }
+]);
